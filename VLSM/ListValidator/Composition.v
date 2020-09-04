0322a0778b448e41a57897b37bda2d34e0eb2088
mcalancea <mihai@journey.localdomain>
2020-09-04 11:19:14 +0300

    Started work on composition.

---
+++ VLSM/ListValidator/Composition.v
@@ -0,0 +1,32 @@
+Require Import Bool List ListSet Reals FinFun RelationClasses Relations Relations_1 Sorting.
+Require Import Lia.
+Import ListNotations.
+From CasperCBC
+Require Import
+  Lib.Preamble
+  Lib.ListExtras
+  Lib.SortedLists
+  VLSM.Common
+  VLSM.Composition
+  VLSM.Equivocation
+  VLSM.ListValidator.ListValidator
+  VLSM.ListValidator.Equivocation
+  VLSM.ObservableEquivocation
+  CBC.Common
+  CBC.Equivocation.
+
+Section Composition.
+
+Context
+  {index : Type}
+  {i0 : index}
+  {index_listing : list index}
+  {Hfinite : Listing index_listing}
+  {idec : EqDec index}
+  (IM_index := fun (i : index) => @VLSM_list index i index_listing idec)
+  {constraint : composite_label IM_index -> (composite_state IM_index) * option (@message index index_listing) -> Prop}
+  (X := composite_vlsm IM_index i0 constraint).
+  
+  
+  
+End Composition.
