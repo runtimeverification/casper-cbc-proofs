7ba49ac03e0f1b52f429d76914683da36597d9f0
mcalancea <mihai@journey.localdomain>
2020-09-04 11:37:57 +0300

    More stuff in context.

--- VLSM/ListValidator/Composition.v
+++ VLSM/ListValidator/Composition.v
@@ -25,7 +25,17 @@ Context
   {idec : EqDec index}
   (IM_index := fun (i : index) => @VLSM_list index i index_listing idec)
   {constraint : composite_label IM_index -> (composite_state IM_index) * option (@message index index_listing) -> Prop}
-  (X := composite_vlsm IM_index i0 constraint).
+  (X := composite_vlsm IM_index i0 constraint)
+  (preX := pre_loaded_vlsm X)
+  (Hevidence : forall (i : index),
+    computable_observable_equivocation_evidence
+        (vstate (IM_index i)) 
+        index 
+        (@state index index_listing) 
+        state_eq_dec 
+        (@comparable_states index index_listing idec state_eq_dec)).
+  
+  
   
   
   
